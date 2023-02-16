eb39b0b2e63e6b563ae80e31a18024889d43f4ad
TobiasOlsson99 <tobiasol@student.chalmers.se>
2023-02-16 11:34:05 +0100

    Removed unused code, cleaned up code, documented some of it

--- web-app/gunshot.py
+++ web-app/gunshot.py
@@ -1,3 +1,5 @@
+from __future__ import annotations #Crazy hack
+
 import geopy.distance
 import math
 import sys
@@ -5,25 +7,92 @@ import os.path
 import os
 import scipy.optimize
 import numpy as np
-import itertools
 
-MAXDISTANCE = 1000 #meters
-MAXTIME = 3000 #Maximum time in milliseconds between different clients hearing a sound
+    #Maximum distance a gunshot can be picked up from
+MAX_DISTANCE = 1000 
+    #Speed of sound in meters per second
 SPEED_OF_SOUND = 343
+    #Speed of sound in meters per millisecond
 SPEED_OF_SOUND_MS = 343/1000
-Pos = tuple[float, float, float]
+    #Maximum difference of time in milliseconds between when two different clients pick up a gunshot
+MAX_TIME_DIFF = MAX_DISTANCE/SPEED_OF_SOUND_MS
+
+
+class Position:
+    def __init__(self, latitude, longitude, altitude):
+        self.v = (latitude, longitude, altitude)
+
+    def __str__(self):
+        return self.v.__str__()
+
+    @property
+    def spherical(self):
+        return self.v[:2]
+
+    @property
+    def latitude(self):
+        return self.v[0]
+
+    @property
+    def longitude(self):
+        return self.v[1]
+
+    @property
+    def altitude(self):
+        return self.v[2]
+
+    def distance(self, pos : Position):
+        #Calculate distance in meters from just latitude and longitude
+        geodesic = geopy.distance.geodesic(self.spherical, pos.spherical).m
+        # Take altitude into account using pythagorean theorem
+        # This will not be accurate for very large distances due
+        # to earth's curvature but will work for these purposes
+        # with (relatively) short distances
+        return math.sqrt(geodesic**2 + (self.altitude - pos.altitude)**2)
+
+    def midpoint(positions : list[Position]):
+        n = len(positions)
+        # Again, this will be accurate for these
+        # (relatively) short distances
+        return Position(sum(p.latitude for p in positions) / n, 
+                        sum(p.longitude for p in positions) / n, 
+                        sum(p.altitude for p in positions) / n)
+
+    def tdoa(positions : list[Position], timestamps : list[int]):
+        n = len(positions)
+        # The objective functions to minimize using Nelder-Mead algorithm
+        # It is a summation of errors squared e_0^2 + e_1^2 + ... + e_n^2
+        # where error e_i is given by 
+        # e_i = distance between position P_x and P_i - distance traveled by sound between timestamp T_x and T_i
+        # Where position P_x and T_x are the variables and P_i and T_i are positions and timestamp of
+        # gunshot report i
+        def objective(x):
+            return sum((positions[i].distance(Position(*x[:3])) - SPEED_OF_SOUND_MS*(timestamps[i]-x[3]))**2 for i in range(n))
+        # Starting guess P_0 is midpoint of positions and T_0 earliest gunshot report timestamp
+        x0 = Position.midpoint(positions).v + (min(timestamps),)
+        # Bounds on latitude and longitude
+        bounds = ((-90,90),(-180,180),(None,None),(None,None))
+        # Minimize using scipy's optimization library
+        sol = scipy.optimize.minimize(objective, x0=x0, method="Nelder-Mead", bounds=bounds, tol=10^-4).x
+
+        #print("Minimization solution:",sol,"Objective value:",objective(sol))
+        return Position(*sol[:3]),int(sol[3])
 
 class GunshotReport:
-    def __init__(self, position: Pos, timestamp: int, weapontype: str, clientid: int):
+    def __init__(self, position : Position, timestamp: int, weapontype: str, clientid: int):
         self.position = position
         self.timestamp = timestamp
         self.weapontype = weapontype
         self.clientid = clientid
 
+    @classmethod
+    def from_coordinates(cls, coordinates : tuple[float, float, float], timestamp: int, weapontype: str, clientid: int):
+        return cls(Position(*coordinates), timestamp, weapontype, clientid)
+
     def description(self):
         return ("Gunshot of type: " + self.weapontype + " detected at time: " + str(self.timestamp) + 
-            " by client " + str(self.clientid) + " at position: " + str(self.position[0]) + ", " + 
-            str(self.position[1]) + " at altitude: " + str(self.position[2]))
+            " by client " + str(self.clientid) + " at position: " + str(self.position.latitude) + ", " + 
+            str(self.position.longitude) + " at altitude: " + str(self.position.altitude))
 
 class GunshotEvent:
     def __init__(self, gunshot: GunshotReport):
@@ -35,8 +104,8 @@ class GunshotEvent:
 
     def try_sameclient(self, gunshot: GunshotReport):
         if (gunshot.clientid in self.clients and
-            gunshot.weapontype == self.weapontype and
-            self.inside_range(gunshot)): #Should always be true since same client is sending
+                gunshot.weapontype == self.weapontype and
+                self.inside_range(gunshot)):
             self.gunshots.append(gunshot)
             self.timestamp_latestshot = max(self.timestamp_latestshot, gunshot.timestamp)
             self.timestamp_firstshot = min(self.timestamp_firstshot, gunshot.timestamp)
@@ -45,7 +114,7 @@ class GunshotEvent:
 
     def try_newclient(self, gunshot: GunshotReport):
         if (gunshot.weapontype == self.weapontype and
-            self.inside_range(gunshot)):
+                self.inside_range(gunshot)):
             self.gunshots.append(gunshot)
             self.clients.add(gunshot.clientid)
             self.timestamp_latestshot = max(self.timestamp_latestshot, gunshot.timestamp)
@@ -55,7 +124,7 @@ class GunshotEvent:
 
     def inside_range(self, gunshot: GunshotReport):
         for gs in self.gunshots:
-            if CoordinateCalculator.distance(gunshot.position, gs.position) > MAXDISTANCE*2:
+            if gunshot.position.distance(gs.position) > MAX_DISTANCE*2:
                 return False
         return True
 
@@ -68,121 +137,12 @@ class GunshotEvent:
                 clients.add(gunshot.clientid)
 
     def approximations(self):
-        if len(self.gunshots) < 3:
+        first_reports = list(self.get_first_reports()) #Only the report of the first gunshot heard, not following gunshots
+        if len(first_reports) < 3:
             return None, None
-        first_reports = list(self.get_first_reports())[:10]
-        ref = first_reports[0]
-        cart_reports = [(CoordinateCalculator.to_cart(ref.position, r.position),r.timestamp) for r in first_reports]
-        print(cart_reports)
-        combs = itertools.combinations(cart_reports, 3)
-        self.position = CoordinateCalculator.from_cart(ref.position,
-                        CoordinateCalculator.midpoint([
-                        CoordinateCalculator.tdoa(*comb) for comb in combs]))
-        self.timestamp = ref.timestamp - int(CoordinateCalculator.distance(ref.position, self.position) * 1000 / SPEED_OF_SOUND)
+        self.position, self.timestamp = Position.tdoa([r.position for r in first_reports], [r.timestamp for r in first_reports])
         return self.position, self.timestamp
 
-    def approximations2(self):
-        if len(self.gunshots) < 3:
-            return None, None
-        first_reports = list(self.get_first_reports())[:10]
-        ref = first_reports[0]
-        data = []#[CoordinateCalculator.to_cart(ref.position, r.position) for r in first_reports]
-        for r in first_reports:
-            x,y,z = CoordinateCalculator.to_cart(ref.position, r.position)
-            data.append((x,y,z,r.timestamp))
-        self.position, self.timestamp = CoordinateCalculator.tdoa(np.array(data))
-        self.position = CoordinateCalculator.from_cart(ref.position,self.position)
-        return self.position, self.timestamp
-    
-    def approximations3(self):
-        if len(self.gunshots) < 3:
-            return None, None
-        first_reports = list(self.get_first_reports())[:10]
-        data = []
-        for r in first_reports:
-            x,y,z = r.position
-            data.append((x,y,z,r.timestamp))
-        self.position, self.timestamp = CoordinateCalculator.tdoa_spherical(np.array(data))
-        return self.position, self.timestamp
-
-class CoordinateCalculator:
-    def distance(pos1: Pos, pos2: Pos):
-        #Calculate distance in meters from just latitude and longitude
-        flat_distance = geopy.distance.distance(pos1[:2], pos2[:2]).m
-        #Take altitude into account using pythagorean theorem
-        #This will not be accurate for very large distances due
-        #to earth's curvature but will work for these purposes
-        #with (relatively) short distances
-        return math.sqrt(flat_distance**2 + (pos1[2] - pos2[2])**2)
-
-    def cart_distance(pos1: Pos, pos2: Pos):
-        return math.sqrt(sum((a-b)**2 for (a,b) in zip(pos1,pos2)))
-
-    def from_cart(ref : Pos, cart : Pos):
-        return (geopy.distance.distance(meters=cart[0]).destination(ref[:2],bearing=0)[0],
-                geopy.distance.distance(meters=cart[1]).destination(ref[:2],bearing=90)[1],
-                ref[2]+cart[2])
-
-    def to_cart(ref : Pos, spherical : Pos):
-        return (geopy.distance.distance(ref[:2],(spherical[0],ref[1])).m * np.sign(spherical[0]-ref[0]),
-                geopy.distance.distance(ref[:2],(ref[0],spherical[1])).m * np.sign((spherical[1]-ref[1] % 360) - 180),
-                spherical[2] - ref[2])
-
-    def tdoa_spherical(data):
-        def objective(x):
-            return sum((CoordinateCalculator.distance(x[:3],xi[:3]) - SPEED_OF_SOUND_MS*(xi[3]-x[3]))**2 for xi in data)
-        x0 = np.sum(data,axis=0)/len(data)
-        bounds = ((-90,90),(-180,180),(None,None),(None,None))
-        sol = scipy.optimize.minimize(objective, x0=x0, method="Nelder-Mead", bounds=bounds, tol=10^-3).x
-        print("Solution:")
-        print(sol,objective(sol))
-        return sol[:3],int(sol[3])
-
-    def tdoa(data):
-        #print(gs1,gs2,gs3)
-        #Using least squares linear approximation
-        def linear_approximate():
-            print("Data:\n", data)
-            n = len(data)
-            A = np.zeros((n*(n-1)//2,4))
-            b = np.zeros((n*(n-1)//2,1))
-            for k,(i,j) in enumerate((i,j) for i in range(n) for j in range(n) if j>i):
-                A[k,:] = 2*(data[j]-data[i])
-                A[k,-1] *= -SPEED_OF_SOUND_MS**2
-                sqrs = np.square(data[j]) - np.square(data[i])
-                sqrs[-1] *= -SPEED_OF_SOUND_MS**2
-                b[k,0] = sum(sqrs)
-            print("A:\n", A)
-            print("b:\n", b)
-            print("Linear estimate:\n",np.linalg.lstsq(A,b,rcond=None)[0][:,0])
-            print(np.linalg.lstsq(A,b,rcond=None)[1])
-            return np.linalg.lstsq(A,b,rcond=None)[0][:,0]
-
-        def objective(x):
-            return sum((CoordinateCalculator.cart_distance(x[:3],xi[:3]) - SPEED_OF_SOUND_MS*(xi[3]-x[3]))**2 for xi in data)
-        def jacobian(x):
-            return [
-                    sum(2 * (CoordinateCalculator.cart_distance(x[:3],xi[:3]) - SPEED_OF_SOUND_MS*(xi[3]-x[3])) *
-                    (x[i]-xi[i]) / CoordinateCalculator.cart_distance(x[:3],xi[:3])
-                    for xi in data)
-                    for i in range(3)
-                ] + [
-                    sum(2 * (CoordinateCalculator.cart_distance(x[:3],xi[:3]) - SPEED_OF_SOUND_MS*(xi[3]-x[3])) *
-                    SPEED_OF_SOUND_MS
-                    for xi in data)
-                ]
-        init = linear_approximate()
-        print(objective(init))
-        sol = scipy.optimize.minimize(objective, [43, -100, 4664,  -200], method="Nelder-Mead", tol=10^-3).x
-        print("Solution:")
-        print(sol,objective(sol))
-        return sol[:3],sol[3]
-
-    def midpoint(poslist):
-        lats, longs, alts = zip(*poslist)
-        l = len(poslist)
-        return (sum(lats)/l, sum(longs)/l, sum(alts)/l)
-            
 #------------TESTING-----------
 
 def popevent(event):
@@ -197,16 +157,16 @@ def popevent(event):
     if len(event.clients) < 3:
         print("\tPosition not able to be pinpointed due to too few reports")
     else:
-        pos, time = event.approximations2()
+        pos, time = event.approximations()
         print("Approximate position: " + str(pos) + " timestamp: " + str(time))
 
 def testfile(gunshotstrings):
     events = []
     for line in gunshotstrings:
         latitude, longitude, altitude, timestamp, weapontype, clientid = line.split(" ")[:6]
-        gunshot = GunshotReport((float(latitude), float(longitude), float(altitude)), int(timestamp), weapontype, int(clientid))
-        [popevent(event) for event in events if gunshot.timestamp - event.timestamp_latestshot > MAXTIME]
-        events = [event for event in events if gunshot.timestamp - event.timestamp_latestshot <= MAXTIME]
+        gunshot = GunshotReport.from_coordinates((float(latitude), float(longitude), float(altitude)), int(timestamp), weapontype, int(clientid))
+        [popevent(event) for event in events if gunshot.timestamp - event.timestamp_latestshot > MAX_TIME_DIFF]
+        events = [event for event in events if gunshot.timestamp - event.timestamp_latestshot <= MAX_TIME_DIFF]
         for event in events:
             if event.try_sameclient(gunshot) == True:
                 break
