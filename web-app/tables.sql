33a945ce4d07a5bfe84751211d95dbdeaac9a0ac
Lukas Gartman <gusgartlu@student.gu.se>
2023-02-21 16:34:52 +0100

    Remove ammunition from database

--- web-app/tables.sql
+++ web-app/tables.sql
@@ -3,14 +3,7 @@ USE pagd;
 
 CREATE TABLE Guns(
     name VARCHAR(255) PRIMARY KEY,
-    type VARCHAR(255) NOT NULL,
-    caliber VARCHAR(255) NOT NULL
-);
-
-CREATE TABLE Ammunitions(
-    size VARCHAR(255),
-    type VARCHAR(255),
-    PRIMARY KEY (size, type)
+    type VARCHAR(255) NOT NULL
 );
 
 CREATE TABLE Reports(
@@ -18,19 +11,13 @@ CREATE TABLE Reports(
     timestamp DATETIME(3) DEFAULT NOW(3) NOT NULL,
     coord VARCHAR(255) NOT NULL,
     gun VARCHAR(255) NOT NULL,
-    bullet_size VARCHAR(255) NOT NULL,
-    bullet_type VARCHAR(255) NOT NULL,
-    FOREIGN KEY (gun) REFERENCES Guns(name),
-    FOREIGN KEY (bullet_size, bullet_type) REFERENCES Ammunitions(size, type)
+    FOREIGN KEY (gun) REFERENCES Guns(name)
 );
 
 CREATE TABLE Gunshots(
     timestamp DATETIME(3) NOT NULL,
     coord VARCHAR(255) NOT NULL,
     gun VARCHAR(255) NOT NULL,
-    bullet_size VARCHAR(255) NOT NULL,
-    bullet_type VARCHAR(255) NOT NULL,
     PRIMARY KEY (timestamp, coord),
-    FOREIGN KEY (gun) REFERENCES Guns(name),
-    FOREIGN KEY (bullet_size, bullet_type) REFERENCES Ammunitions(size, type)
+    FOREIGN KEY (gun) REFERENCES Guns(name)
 );
