b22b85bd31aebc94efc45aa603b8c0bb01c997bb
Lukas Gartman <gusgartlu@student.gu.se>
2023-04-16 02:43:25 +0200

    Create inserts.sql
    Former-commit-id: 2606347e1e02d4695c63f7864c4d865974173c71

---
+++ web-app/inserts.sql
@@ -0,0 +1,21 @@
+-- Handguns --
+INSERT IGNORE INTO Guns (name, type) VALUES
+    ("Colt 1911", "Handgun"),
+    ("Glock", "Handgun"),
+    ("HK USP", "Handgun"),
+    ("Kimber", "Handgun"),
+    ("Lorcin", "Handgun"),
+    ("Ruger", "Handgun"),
+    ("SIG Sauer", "Handgun"),
+    ("SpKing", "Handgun"),
+    ("SW22", "Handgun"),
+    ("SW38sp", "Handgun");
+
+-- Rifles --
+INSERT IGNORE INTO Guns (name, type) VALUES
+    ("Bolt action", "Rifle"),
+    ("M16", "Rifle"),
+    ("MP40", "Rifle"),
+    ("Remington 700", "Rifle"),
+    ("Win M14", "Rifle"),
+    ("WASR", "Rifle");
