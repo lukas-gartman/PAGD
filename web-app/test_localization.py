b2e3b725ea7ca05a87c0b82d7f5bffdd28afe3bb
TobiasOlsson99 <tobiasol@student.chalmers.se>
2023-04-27 11:39:40 +0200

    Small fixes to testing algorithm
    Former-commit-id: dd365b0dae4dd98881e4b3d272dd3925eae6b637

--- web-app/test_localization.py
+++ web-app/test_localization.py
@@ -1,11 +1,11 @@
-from gunshot import Position, SPEED_OF_SOUND_MS
+from gunshot import Position, GunshotEvent, GunshotReport, SPEED_OF_SOUND_MS
 import requests, time, random, geopy.distance
 
 url = "http://lukas.tottes.net"
     
 class Test:
-    def __init__(self, start_timestamp, max_timestamp_error = 100, max_position_error = 15, client_amount = (3,8),
-                 client_distance = (0,500), client_heard = 0.9):
+    def __init__(self, start_timestamp, max_timestamp_error = 100, max_position_error = 15, client_amount = (8,8),
+                 client_distance = (50,500), client_heard = 0.9, local = False):
         self.start_timestamp = start_timestamp
         self.gunshots_fired = 0
         self.max_timestamp_error = max_timestamp_error
@@ -13,11 +13,15 @@ class Test:
         self.client_amount = client_amount
         self.client_distance = client_distance
         self.client_heard = client_heard
+        self.local = local
 
         self.gunshot_lat, self.gunshot_long = random.uniform(54, 69), random.uniform(10, 26)
         self.gunshot_pos = Position(self.gunshot_lat, self.gunshot_long, 0)
 
         self.clients = [Client(self) for _ in range(random.randint(*self.client_amount))]
+
+        if not local:
+            self.token = Client.get_token()
 
     def run(self, gunshot_amount = 1):
         for i in range(gunshot_amount):
@@ -26,6 +30,9 @@ class Test:
             self.gunshots_fired += 1
         
     def collect_results(self):
+        if self.local:
+            return self.collect_results_local()
+
         headers = {
             "Authorization": self.token
         }
@@ -37,17 +44,27 @@ class Test:
         if not (resp.status_code >= 200 and resp.status_code < 300):
             raise Exception(resp)
         
-        events = resp.json()
-        if len(events) == 0:
+        event = resp.json()
+        print(event)
+        if len(event) == 0:
             print("Could not find event")
-        elif len(events) > 1:
+            return None
+        elif "nånting" in event.keys():
             print("Too many events")
+            return None
         else:
-            event = events[0]
             event_pos = Position(event["coord_lat"], event["coord_long"], event["coord_alt"])
             print(f"Error in meters: {self.gunshot_pos.distance(event_pos)}")
-            if self.gunshots_fired != event["shots_fired"]:
-                print(f"Shots fired: {self.gunshots_fired}, heard: {event['shots_fired']}")
+            return self.gunshot_pos.distance(event_pos)
+        
+    def collect_results_local(self):
+        pos, timestamp = self.event.approximations()
+        if pos is None:
+            print("Could not determine position")
+            return None
+        else:
+            print(f"Error in meters: {self.gunshot_pos.distance(pos)}")
+            return self.gunshot_pos.distance(pos)
 
 class Client:
     def __init__(self, test : Test):
@@ -61,26 +78,31 @@ class Client:
                                                                theta=random.uniform(0,360),
                                                                phi=random.uniform(-10,10))
         
-        self.time_to_arrival = int(test.gunshot_pos.distance(self.true_pos) * SPEED_OF_SOUND_MS)
+        self.time_to_arrival = int(test.gunshot_pos.distance(self.true_pos) / SPEED_OF_SOUND_MS)
 
-        self.token = Client.get_token()
+        if not test.local:
+            self.token = Client.get_token()
+        else:
+            self.id = random.randint(0,100000000000)
 
     def sound_wave(self, gunshot_timestamp):
         if self.first_report or random.uniform(0,1) <= self.test.client_heard:
             self.first_report = False
-            self.send_report(gunshot_timestamp + self.time_to_arrival + random.randrange(0,self.test.max_timestamp_error))
-            input()
+            if not self.test.local:
+                self.send_report(gunshot_timestamp + self.time_to_arrival + random.randrange(0,self.test.max_timestamp_error))
+            else:
+                self.send_report_local(gunshot_timestamp + self.time_to_arrival + random.randrange(0,self.test.max_timestamp_error))
 
     def get_token():
         data = requests.get(url + "/register")
         return data.json()["token"]
     
-    def send_report(self, timestamp):
+    def send_report(self, report_timestamp):
         headers = {
             "Authorization": self.token
         }
         json = {
-            "timestamp": timestamp,
+            "timestamp": report_timestamp,
             "coord_lat": self.gps_pos.latitude,
             "coord_long": self.gps_pos.longitude,
             "coord_alt": self.gps_pos.altitude,
@@ -89,8 +111,19 @@ class Client:
         resp = requests.post(url + "/api/reports", json=json, headers=headers)
         if not (resp.status_code >= 200 and resp.status_code < 300):
             raise Exception(resp.text)
+        
+    def send_report_local(self, report_timestamp):
+        report = GunshotReport(self.gps_pos, report_timestamp, "AR-15", self.id)
+        if not hasattr(self.test, "event"):
+            self.test.event = GunshotEvent(report)
+        else:
+            if self.test.event.fits(report):
+                self.test.event.add_report(report)
+            else:
+                print("Report does not fit in event")
 
 if __name__ == "__main__":
-    test = Test(time.time() * 1000)
-    test.run(1)
-    test.collect_results()
+    while True:
+        test = Test(int(time.time() * 1000), local=False)
+        test.run(1)
+        test.collect_results()
